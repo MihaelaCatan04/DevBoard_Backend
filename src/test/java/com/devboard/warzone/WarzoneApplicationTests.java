4f223c1193fd135de897c8cff56cbb1a7ccf31f8
Mihaela Catan <mihaelacatan7@gmail.com>
2026-05-10 21:00:49 +0300

    Initial commit

---
+++ src/test/java/com/devboard/warzone/WarzoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devboard.warzone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarzoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
